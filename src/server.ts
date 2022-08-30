592cbfcef793e00772eb917f544958996c795e47
Sadik <sadikbi1985@gmail.com>
2022-08-30 10:04:24 +0300

    todo task updated

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,19 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
+  app.get('/filteredimage', async(req,res)=>{
+    const image_url=req.query.image_url.toString();
+    if (!image_url){
+      res.status(422).send('Error : Empty  url ');
+    }else{
+      const filteredimages=await filterImageFromURL(image_url);
+      res.status(200).sendFile(filteredimages, () => {       
+        deleteLocalFiles([filteredimages]);
+    });
+
+  }
+});
+
   /**************************************************************************** */
 
   //! END @TODO1
